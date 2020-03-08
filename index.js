7d86d5f9ac23311814511aeea2262eb434533617
Cleudson <cleudsoncunha@gmail.com>
2020-03-08 17:18:42 -0300

    change method names

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const applyMask = require('./src/fn/applyMask');
 module.exports = function mdg ({pattern, onError, onSuccess, validation}){
   let args = {pattern, onError, onSuccess, validation};
-  const check = (input) => applyMask({input, ...args});
+  const checkValue = (input) => applyMask({input, ...args});
   const checkField = (inputField, eventType = 'input') => {
     inputField.addEventListener(eventType , function (event){
-        event.target.value = check(event.target.value)['output'];
+        event.target.value = checkValue(event.target.value)['output'];
     })
   }
-  const settings = {...args, check, checkField}
+  const settings = {...args, checkValue, checkField}
   Object.freeze(settings);
   return settings;
 };
