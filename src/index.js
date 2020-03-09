079d8661bc8b594b6c4493b8e0838c27acb348c3
Cleudson <cleudsoncunha@gmail.com>
2020-03-09 14:59:26 -0300

    change folder structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const applyMask = require('./src/fn/applyMask');
+const applyMask = require('./fn/applyMask');
 module.exports = function mdg ({pattern, onError, onSuccess, validation}){
   let args = {pattern, onError, onSuccess, validation};
   const checkValue = (input) => applyMask({input, ...args});
